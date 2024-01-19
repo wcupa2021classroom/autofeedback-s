e6e62ad2cf98137c0fdbd31cba59a713226602fc
David Cooper <dcooper@CooperLab-2.local>
2024-01-19 13:58:46 -0500

    v1.1

--- dist/index.js
+++ dist/index.js
@@ -13687,7 +13687,7 @@ const runCommand = async (test, cwd, timeout) => {
     await waitForExit(child, timeout);
     // Eventually work off the the test type
     if ((!test.output || test.output == '') && (!test.input || test.input == '')) {
-        return;
+        return output;
     }
     const expected = normalizeLineEndings(test.output || '');
     const actual = normalizeLineEndings(output);
