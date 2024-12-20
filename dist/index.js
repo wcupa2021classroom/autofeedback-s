7aadc89078da3ab99f34c7a989806243b9331939
David Cooper <dcooper@wcupa.edu>
2024-12-20 08:43:09 -0500

    v1.2test (#33)

--- dist/index.js
+++ dist/index.js
@@ -13562,6 +13562,7 @@ const core = __importStar(__webpack_require__(470));
 const output_1 = __webpack_require__(52);
 const os = __importStar(__webpack_require__(87));
 const chalk_1 = __importDefault(__webpack_require__(843));
+const fuzzySearch_1 = __webpack_require__(953);
 const color = new chalk_1.default.Instance({ level: 1 });
 class TestError extends Error {
     constructor(message) {
@@ -13728,7 +13729,7 @@ const runCommand = async (test, cwd, timeout) => {
     }
     const expected = normalizeLineEndings(test.output || '');
     const actual = normalizeLineEndings(output);
-    const diffMessage = (actual, expected) => {
+    const exactDiffMessage = (actual, expected) => {
         const linesActual = actual.split(/\r?\n/);
         const linesExpected = expected.split(/\r?\n/);
         const minLines = Math.min(linesActual.length, linesExpected.length);
@@ -13743,7 +13744,7 @@ const runCommand = async (test, cwd, timeout) => {
         result.push(`Num lines to test ` + linesExpected.length);
         result.push(`  Num lines total ` + linesActual.length);
         if (linesExpected.length > linesActual.length) {
-            result.push(` mising ` + (linesExpected.length - linesActual.length) + ` lines of output`);
+            result.push(` missing ` + (linesExpected.length - linesActual.length) + ` lines of output`);
         }
         else if (linesExpected.length < linesActual.length) {
             result.push(` extra ` + (linesActual.length - linesExpected.length) + ` lines of output`);
@@ -13757,9 +13758,8 @@ const runCommand = async (test, cwd, timeout) => {
         let actualLine = ``;
         result.push(``);
         // Look at each line
-        let i;
         if (linesExpected.length == linesActual.length) {
-            for (i = 0; i < minLines; i++) {
+            for (let i = 0; i < minLines; i++) {
                 expectedLine = linesExpected[i];
                 actualLine = linesActual[i];
                 if (actualLine == expectedLine) {
@@ -13811,11 +13811,37 @@ const runCommand = async (test, cwd, timeout) => {
         }
         return result.join(os.EOL);
     };
+    const includedDiffMessage = (actual, expected) => {
+        const actualLines = actual.split(/\r?\n/);
+        const result = ['  '];
+        result.push('');
+        result.push('Full program output:');
+        result.push(actual);
+        result.push('');
+        result.push('Included string expected for this test:');
+        result.push(expected);
+        result.push('');
+        const closest = (0, fuzzySearch_1.fuzzySearch)(actual, expected);
+        result.push(`🟥------- Expected text not found `);
+        result.push('');
+        result.push('🟥EXPECTED: "' + expected + '"');
+        // We do not want to consider line endings in the number in character counts
+        const closestIndex = actual.replace(/\r?\n/g, '').indexOf(closest);
+        let charCount = 0;
+        let currLine = 1;
+        while (charCount < closestIndex) {
+            charCount += actualLines[currLine - 1].length;
+            currLine++;
+        }
+        result.push('🟥 CLOSEST: "' + closest + '" starting on line ' + currLine + ' pos ' + closestIndex);
+        result.push('');
+        return result.join(os.EOL);
+    };
     switch (test.comparison) {
         case 'exact':
             if (actual != expected) {
                 //core.group(`Error: ${test.name}`, async() => {
-                const result = diffMessage(actual, expected);
+                const result = exactDiffMessage(actual, expected);
                 throw new TestError(`The output for test ${test.name} does not match:
 ${result}`);
                 //throw new TestOutputError(`The output for test ${test.name} did not match`, expected, actual)
@@ -13834,7 +13860,7 @@ ${result}`);
             // The default comparison mode is 'included'
             if (!actual.includes(expected)) {
                 //core.group(`Error: ${test.name}`, async() => {
-                const result = diffMessage(actual, expected);
+                const result = includedDiffMessage(actual, expected);
                 throw new TestError(`The output for test ${test.name} did not match:
 ${result}`);
                 //throw new TestOutputError(`The output for test ${test.name} did not match`, expected, actual)
@@ -31454,6 +31480,104 @@ function checkBypass(reqUrl) {
     return false;
 }
 exports.checkBypass = checkBypass;
+
+
+/***/ }),
+
+/***/ 953:
+/***/ (function(__unusedmodule, exports) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fuzzySearch = void 0;
+/** Performs a fuzzy search over string input to find the closest matching item */
+const fuzzySearch = (input, toFind) => {
+    const windows = toWindows(input.replace(/\r?\n/g, ' '), toFind.length);
+    const firstDistance = [0, jaroWinklerSimilarity(windows[0], toFind)];
+    const closestIndex = windows.reduce((prev, curr, index) => {
+        const score = jaroWinklerSimilarity(curr, toFind);
+        return prev[1] < score ? [index, score] : prev;
+    }, firstDistance)[0];
+    return windows[closestIndex];
+};
+exports.fuzzySearch = fuzzySearch;
+/**
+ * Naive implementation to create windows over the input string
+ * Returned array is of size N - S + 1 where N is the amount of characters in the string
+ * and S is the required size of the window
+ *
+ * If the input is smaller than the requested size, an array containing
+ * the input will be returned
+ */
+const toWindows = (input, size) => {
+    if (size > input.length) {
+        return [input];
+    }
+    const result = [];
+    const lastWindow = input.length - size;
+    for (let i = 0; i <= lastWindow; i++) {
+        result.push(input.slice(i, i + size));
+    }
+    return result;
+};
+/**
+ * Calculates the Jaro-Winkler Similarity between two strings.
+ * The Jaro Similarity value range is from 0 to 1 where 0 means there is no similarity and 1 means they are equal.
+ *
+ * Then the Jaro-Winkler Similarity is calculated by multiplying the length of a common prefix up to four characters to
+ * a constant `p`
+ * Algorithm described here: https://en.wikipedia.org/wiki/Jaro%E2%80%93Winkler_distance
+ */
+const jaroWinklerSimilarity = (str1, str2) => {
+    if (str1 == str2)
+        return 1.0;
+    const len1 = str1.length;
+    const len2 = str2.length;
+    // Max distance between characters to be considered matching
+    // This will cause inaccuracies with shorter words (min length for some accuracy is 4)
+    const maxDist = Math.floor(Math.max(len1, len2) / 2) - 1;
+    let matches = 0;
+    let transpositions = 0;
+    const str1Matches = Array(len1).fill(false);
+    const str2Matches = Array(len2).fill(false);
+    // Iterate through every character of str1
+    for (let i = 0; i < len1; i++) {
+        // Iterate over a window of characters in str2 with a max width of maxDist * 2
+        for (let j = Math.max(0, i - maxDist); j < Math.min(len2, i + maxDist + 1); j++) {
+            // If the characters are equal and the second has not been matched yet, consider them a match
+            if (str1.charAt(i) === str2.charAt(j) && !str2Matches[j]) {
+                str1Matches[i] = true;
+                str2Matches[j] = true;
+                matches += 1;
+                // Found a match! Break and move to the next character
+                break;
+            }
+        }
+    }
+    // Return 0 if not a single match was found. Considered to have no similarity
+    if (matches == 0)
+        return 0;
+    let k = 0;
+    // Go through the matches and calculate the total transpositions
+    for (let i = 0; i < len1; i++) {
+        if (str1Matches[i]) {
+            while (!str2Matches[k])
+                k++;
+            if (str1.charAt(i) != str2.charAt(k++))
+                transpositions++;
+        }
+    }
+    transpositions /= 2;
+    const jaro = (matches / len1 + matches / len2 + (matches - transpositions) / matches) / 3.0;
+    let prefixLength = 0;
+    for (let i = 0; i < Math.min(str1.length, str2.length, 4); i++) {
+        if (str1.charAt(i) == str2.charAt(i))
+            prefixLength++;
+    }
+    // 0.1 is based on Winkler's original work. Can be any value in the range (0, 0.25]
+    return jaro + prefixLength * 0.1 * (1 - jaro);
+};
 
 
 /***/ }),
