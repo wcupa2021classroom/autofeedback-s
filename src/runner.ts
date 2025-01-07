a11f0194073057625c7d88f719679dad62b3e1f5
David Cooper <dcooper@wcupa.edu>
2025-01-06 22:10:41 -0500

    better messages (#34)
    * better messages

--- src/runner.ts
+++ src/runner.ts
@@ -205,8 +205,19 @@ const runCommand = async (test: Test, cwd: string, timeout: number) => {
     child.stdin.write(test.input)
     child.stdin.end()
   }
-
-  await waitForExit(child, timeout)
+  try {
+    await waitForExit(child, timeout)
+  } catch (error) {
+    if (error instanceof TestTimeoutError) {
+      throw new TestTimeoutError(`${output}\n${error.message}`)
+    } else if (error instanceof TestError) {
+      throw new TestError(`${output}\n${error.message}`)
+    } else if (error instanceof Error) {
+      throw new Error(`${output}\n${error.message}`)
+    } else {
+      throw new Error(`${output}\nUnknown ERROR`)
+    }
+  }
 
   // Eventually work off the the test type
   if ((!test.output || test.output == '') && (!test.input || test.input == '')) {
